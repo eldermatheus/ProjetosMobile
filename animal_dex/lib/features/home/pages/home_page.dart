c23788b7d3461c0ff168516528496436aa8bdbef
eldermatheus <elder_matheus11@hotmail.com>
2022-04-26 18:01:39 -0300

    Iniciando projeto AnimalDesk

--- animal_dex/lib/features/home/pages/home_page.dart
+++ animal_dex/lib/features/home/pages/home_page.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
+import '../../../common/models/animal.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key, required this.list}) : super(key: key);
+  final List<Animal> list;
 
   @override
   Widget build(BuildContext context){
-    return Scaffold(
-
-    );
+    return Scaffold();
   }
 }
